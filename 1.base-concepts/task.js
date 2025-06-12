16e2bce7483391645eb1d5dc5bf3998adc53f1fa
lewkAa <tihonovtan@gmail.com>
2025-06-12 15:47:05 +0300

    Task 1 update

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,17 +1,19 @@
 "use strict";
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  let result;
-  let descr = (Math.pow(b,2) -4 * a *c);
-  if (descr === 0) {
-  result = -b/(2*a);
-  arr = [result]}
-  if (descr > 0){
-  let first = (-b + Math.sqrt(d) )/(2*a);
-  let second = (-b - Math.sqrt(d) )/(2*a);
-  arr = [first, second]
-  }
-  return arr;
+	let arr = [];
+	let result;
+	let descr = (Math.pow(b, 2) - 4 * a * c);
+	if (descr === 0) {
+		result = -b / (2 * a);
+		arr = [result]
+	}
+	if (descr > 0) {
+		let first = (-b + Math.sqrt(descr)) / (2 * a);
+		let second = (-b - Math.sqrt(descr)) / (2 * a);
+		arr = [first, second]
+	}
+	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
