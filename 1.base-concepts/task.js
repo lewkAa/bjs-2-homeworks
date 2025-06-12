4e1d1b6c5f7b763a7d5f770a4cb3ce24ad43d0ba
lewkAa <tihonovtan@gmail.com>
2025-06-12 15:41:39 +0300

    Task 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,16 @@
-"use strict"
+"use strict";
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let result;
+  let descr = (Math.pow(b,2) -4 * a *c);
+  if (descr === 0) {
+  result = -b/(2*a);
+  arr = [result]}
+  if (descr > 0){
+  let first = (-b + Math.sqrt(d) )/(2*a);
+  let second = (-b - Math.sqrt(d) )/(2*a);
+  arr = [first, second]
+  }
   return arr;
 }
 
