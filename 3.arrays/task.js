cb9027c36b7b02f1dd1bc24b11670e31a6655380
lewkAa <tihonovtan@gmail.com>
2025-06-19 11:42:00 +0300

    hw3 update

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -5,12 +5,8 @@ function compareArrays(arr1, arr2) {
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-let result = users.filter(user => user.gender === gender).map(user => user.age).reduce((acc, item, index,arr) => {
-acc+=item;
-if (index === arr.length-1){
-return acc / arr.length;
-}
-return acc;
-},0)
-  return result;
+  const filteredUsers = users.filter(user => user.gender === gender);
+  return filteredUsers.length === 0
+    ? 0
+    : filteredUsers.reduce((sum, user) => sum + user.age, 0) / filteredUsers.length;
 }
