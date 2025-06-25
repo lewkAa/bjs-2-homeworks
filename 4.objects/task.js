2fd4c7bb41d3c1c495f6eb3d74a8d1893bf9cd72
lewkAa <tihonovtan@gmail.com>
2025-06-25 23:05:39 +0300

    Hw4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,33 @@
+"use strict";
+
 function Student(name, gender, age) {
-  
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function(...marks) {
+	if (this.marks) {
+		this.marks.push(...marks);
+	}
 }
 
-Student.prototype.getAverage = function () {
-  
+Student.prototype.getAverage = function() {
+	if (!this.marks || !Array.isArray(this.marks) || this.marks.length === 0) {
+		return 0;
+	}
+
+	let sum = this.marks.reduce((acc, element) => acc + element, 0);
+	return parseFloat((sum / this.marks.length).toFixed(2));
 }
 
-Student.prototype.exclude = function (reason) {
-  
+Student.prototype.exclude = function(reason) {
+	delete this.subject;
+	delete this.marks;
+	this.excluded = reason;
 }
