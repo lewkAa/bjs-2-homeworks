4e1d9f38dfaa2f55cc1ab83a33f3ccbf272adc1d
lewkAa <tihonovtan@gmail.com>
2025-08-02 14:58:35 +0300

    HW6

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,53 @@
-﻿
+﻿function parseCount(number) {
+	const result = Number.parseFloat(number);
+	if (isNaN(result)) {
+		throw new Error("Невалидное значение")
+	}
+	return result;
+}
+
+function validateCount(value) {
+	try {
+		return parseCount(value);
+	} catch (error) {
+		return error;
+	}
+}
+
+class Triangle {
+	constructor(a, b, c) {
+		if ((a + b) <= c || (b + c) <= a || (a + c) <= b) {
+			throw new Error("Треугольник с такими сторонами не существует");
+		}
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+
+
+	get perimeter() {
+		return this.a + this.b + this.c;
+	}
+
+	get area() {
+		const p = this.perimeter / 2;
+		const area = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+		return parseFloat(area.toFixed(3));
+	}
+
+}
+
+function getTriangle(a, b, c) {
+	try {
+		return new Triangle(a, b, c);
+	} catch (error) {
+		return {
+			get area() {
+				return "Ошибка! Треугольник не существует";
+			},
+			get perimeter() {
+				return "Ошибка! Треугольник не существует";
+			}
+		};
+	}
+}
