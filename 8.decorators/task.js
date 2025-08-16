65a701ea7fb19ae8a0afc9d4a06f8b4b58244a3c
lewkAa <tihonovtan@gmail.com>
2025-08-16 20:52:15 +0300

    HW 8

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,55 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+  let cache = [];
+
+  function wrapper (...args) {
+    const hash = (md5(args)).toString();
+    console.log ("Хэш: " + hash);
+    let objectInCache = cache.find((item) => item.hash === hash);
+    if (objectInCache) {
+      console.log("Из кеша: " + objectInCache.value);
+      return "Из кеша: " + objectInCache.value;
+    }
+   
+    let result = func(...args);
+    cache.push({hash, value: result}); 
+    if (cache.length > 5) {
+      cache.shift();
+    }
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
+  }
+  return wrapper;
 }
+
+
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+  let timeoutId = null;
+  wrapper.allCount = 0;
+  wrapper.count = 0;
+
+
+  function wrapper(...args) {
+    wrapper.allCount++;
+    if (timeoutId ) {
+        console.log("Уже есть таймаут" + args);
+        clearTimeout(timeoutId);
+    }
+    if (!timeoutId) {
+        console.log("Нет таймаута, выполняем функцию" + args); 
+      func.call(this,...args);
+      wrapper.count++;
+    }
+
+    timeoutId = setTimeout(() => {
+      console.log("Задержка больше" + delay);
+      func.apply(this, args);
+      wrapper.count++;
+    }, delay);
+     
 }
+return wrapper;
+}
+ 
